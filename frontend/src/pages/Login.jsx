d490ebb13377ad3ee713c4dcf0493d74e17db6a8
Muhammad A <hotuanminh7268@gmail.com>
2024-11-11 10:49:24 +0500

    Add Login & SignUP Pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div className="w-full absolute -z-10 top-0 left-0" style={{ height: "100vh" }}>
+			<img src="../../src/assets/images/back.jpg" alt="TQ-MAX" width={1920} height={1097} className="w-full h-full object-center" />
+			<div className="rounded-lg shadow-md shadow-gray-300 lg:w-1/4 absolute lg:top-1/2 lg:right-1/4 z-10 bg-white lg:translate-x-1/2 lg:-translate-y-1/2 py-9 px-10">
+				<div className="text-2xl mb-4 font-bold">Login</div>
+				<div className="flex gap-3 mb-7">
+					<div className="text-md">New user?</div>
+					<Link to="/register" className="text-blue-400">
+						Create an account
+					</Link>
+				</div>
+				<div className="flex flex-col gap-3">
+					<div>
+						<div>Email</div>
+						<input type="email" className="border rounded-md outline-none py-1 px-2 w-full hover:border-blue-300 focus:border-blue-300" />
+					</div>
+					<div>
+						<div>Password</div>
+						<input type="password" className="border rounded-md outline-none py-1 px-2 w-full hover:border-blue-300 focus:border-blue-300" />
+					</div>
+					<button className="bg-blue-100 ms-auto px-4 py-2 rounded-md mt-2 hover:bg-blue-200">Login</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
